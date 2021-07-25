9b78be6f4af214c5bf2623ee1138389144eac952
lorenzodallamuta <lorenzodallamuta@gmail.com>
2021-07-25 21:49:21 +0200

    getting colors from the mask

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ I can't think of a way to accomodate all possible input formats that could be us
 a list on my own, even a list that would include the mask samples found in this repo (that sample is in no way restricted... anything could be used in place of its elements and it should still work).
 The and result should be in the form: "white is going to be removed (and if transparency is there probably setting it to 0 would be wise)",
 this means that a png with seemingly proper values could have wrong rgb values even if the transparency us set correctly, it needs to be white.
+
+about getting colors from the mask file:
+save the file in a proper common format (e.g. png 24-bit), seriously it's not worth it to deal with this programmatically
--- main.py
+++ main.py
@@ -19,31 +19,18 @@ if __name__ == '__main__':
 
     df = pd.read_csv(DATASET_PATH, index_col=0)
 
-    text = " ".join(review for review in df.description)
+    uk_rows = df.query("country=='England'").description
+    uk_reviews = (" ".join(review for review in uk_rows))
 
     stopwords = set(STOPWORDS)
     stopwords.update(["drink", "now", "wine", "flavor", "flavors"])
 
-    # loc_mask = np.array(Image.open("masks/loc.png"))
-    # star_mask = np.array(Image.open("masks/star.png"))
-    wine_mask = np.array(Image.open("masks/wine_mask.png"))
-    # keep it 4 bit
-    transformed_wine_mask = np.ndarray(
-        (wine_mask.shape[0], wine_mask.shape[1]), np.int32)
-    for i in range(len(wine_mask)):
-        transformed_wine_mask[i] = list(
-            map(lambda x: 255 if x == 0 else 0, wine_mask[i]))
-    # make it 24 bit
-    # transformed_wine_mask = np.ndarray(
-    #     (wine_mask.shape[0], wine_mask.shape[1], 4), np.int32)
-    # for i in range(len(wine_mask)):
-    #     transformed_wine_mask[i] = list(
-    #         map(lambda x: (255, 255, 255, 0) if x == 0 else (0, 0, 0, 0), wine_mask[i]))
+    uk_mask = np.array(Image.open("masks/flag.png"))
 
-    wordcloud = WordCloud(random_state=0, width=800, height=600, mask=transformed_wine_mask, stopwords=stopwords,
-                          relative_scaling=1, background_color=None, mode="RGBA")
-    # processed_text = wordcloud.process_text(text)
-    # image = wordcloud.generate_from_frequencies(processed_text)
-    image = wordcloud.generate(text)
+    image_colors = ImageColorGenerator(uk_mask)
 
-    image.to_file("wordclouds/wines.png")
+    uk_wordcloud = WordCloud(random_state=0, width=800, height=600, mask=uk_mask,
+                             stopwords=stopwords, background_color="white", color_func=image_colors, mode="RGBA")
+    uk_image = uk_wordcloud.generate(uk_reviews)
+
+    uk_image.to_file("wordclouds/uk_wines.png")
