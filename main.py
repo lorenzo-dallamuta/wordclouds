51b6f8f87192050e49725f9a5f041d1b072a102e
lorenzodallamuta <lorenzodallamuta@gmail.com>
2021-07-25 17:18:04 +0200

    single review test

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from os import path
+# from matplotlib import pyplot as plt
 import pandas as pd
 from waiting import wait
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 from kaggle_.config import api as kaggle_api
 
 DATASET_DIR = 'datasets/'
@@ -15,3 +17,14 @@ if __name__ == '__main__':
              timeout_seconds=120, waiting_for='dataset to download')
 
     df = pd.read_csv(DATASET_PATH, index_col=0)
+
+    text = df.description[0]
+
+    wordcloud = WordCloud(
+        max_words=100, background_color="white").generate(text)
+
+    # plt.imshow(wordcloud, interpolation='bilinear')
+    # plt.axis("off")
+    # plt.show()
+
+    wordcloud.to_file("wordclouds/wines.png")
